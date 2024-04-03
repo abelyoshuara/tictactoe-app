987869c7120e6141d3992611b337af5712a792ef
Abel Yoshuara <abelyoshuara@gmail.com>
2024-04-03 15:04:52 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,68 @@ import Board from "./components/Board";
 import History from "./components/History";
 
 function App() {
-  const [history, setHistory] = useState<(string[] | null[])[]>([
-    Array(9).fill(null),
-  ]);
-  const [isDescOrdered, setIsDescOrdered] = useState<boolean>(false);
-  const [currentMove, setCurrentMove] = useState<number>(0);
-  const xIsNext: boolean = currentMove % 2 === 0;
-  const currentSquares: string[] | null[] =
-    history[currentMove] || Array(9).fill(null);
+  const [tictactoe, setTictactoe] = useState({
+    history: [Array(9).fill(null)],
+    currentSquares: Array(9).fill(null),
+    currentMove: 0,
+    isDescOrdered: false,
+  });
+
+  const xIsNext: boolean = tictactoe.currentMove % 2 === 0;
 
   function handlePlay(nextSquares: string[] | null[]) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
+    const nextHistory = [
+      ...tictactoe.history.slice(0, tictactoe.currentMove + 1),
+      nextSquares,
+    ];
+
+    setTictactoe((prevState) => ({
+      ...prevState,
+      history: nextHistory,
+      currentMove: nextHistory.length - 1,
+      currentSquares: nextSquares,
+    }));
+
     localStorage.setItem("history", JSON.stringify(nextHistory));
   }
 
   function handleJumpTo(nextMove: number) {
-    setCurrentMove(nextMove);
+    setTictactoe((prevState) => ({
+      ...prevState,
+      currentMove: nextMove,
+      currentSquares: prevState.history[nextMove],
+    }));
   }
 
   function clear() {
+    setTictactoe({
+      history: [Array(9).fill(null)],
+      currentSquares: Array(9).fill(null),
+      currentMove: 0,
+      isDescOrdered: false,
+    });
+
     localStorage.removeItem("history");
-    setHistory([Array(9).fill(null)]);
-    setCurrentMove(0);
   }
+
+  const handleSorting = () => {
+    setTictactoe((prevState) => ({
+      ...prevState,
+      isDescOrdered: !prevState.isDescOrdered,
+    }));
+  };
 
   useEffect(() => {
     const savedHistory = JSON.parse(
       localStorage.getItem("history") ||
         "[[null, null, null, null, null, null, null, null, null]]",
     );
-    setHistory(savedHistory);
-    setCurrentMove(savedHistory.length ? savedHistory.length - 1 : 0);
+
+    setTictactoe((prevState) => ({
+      ...prevState,
+      history: savedHistory,
+      currentMove: savedHistory.length ? savedHistory.length - 1 : 0,
+    }));
   }, []);
 
   return (
@@ -49,7 +78,7 @@ function App() {
           <div className="flex flex-col items-center gap-3">
             <Board
               xIsNext={xIsNext}
-              squares={currentSquares}
+              squares={tictactoe.currentSquares}
               onPlay={handlePlay}
             />
           </div>
@@ -64,16 +93,16 @@ function App() {
             <button
               type="button"
               className="text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-cyan-600 dark:hover:bg-cyan-700 focus:outline-none dark:focus:ring-cyan-800"
-              onClick={() => setIsDescOrdered((prevState) => !prevState)}
+              onClick={handleSorting}
             >
-              {isDescOrdered ? "Asc" : "Desc"}
+              {tictactoe.isDescOrdered ? "Asc" : "Desc"}
             </button>
           </div>
           <History
-            moves={history}
+            moves={tictactoe.history}
             onJumpTo={handleJumpTo}
-            currentMove={currentMove}
-            isDescOrdered={isDescOrdered}
+            currentMove={tictactoe.currentMove}
+            isDescOrdered={tictactoe.isDescOrdered}
           />
         </div>
       </div>
